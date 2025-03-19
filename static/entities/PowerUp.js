c9edd3992a1269545c1167902537fee404090fd9
Jack Harris <st_tuff@icloud.com>
2025-03-19 17:08:02 +1100

    Updated the PowerUp to now draw its hitbox correctly at its location and with its correct size of 32x32.

--- static/entities/PowerUp.js
+++ static/entities/PowerUp.js
@@ -4,20 +4,28 @@ class PowerUp extends Entity{
 
     constructor(x,y) {
         super(x,y,26);
-        this._rect = new Rectangle(x,y-16,64,64);
+        // Make the hitbox smaller and position it to match the visual gem
+        // Assuming the gem sprite is roughly 32x32 pixels
+        this._rect = new Rectangle(x, y, 32, 32);
     }
 
     render(ctx){
-        App.drawSprite(this._x*App.getScale(),this._y*App.getScale(),this._textureId);
+        // Draw the sprite
+        App.drawSprite(this._x*App.getScale(), this._y*App.getScale(), this._textureId);
 
         if(App.showDebug()){
+            // Draw the collision box exactly as it's used for collision detection
             ctx.fillStyle = "rgba(255,255,0,0.5)";
-            ctx.fillRect(this._rect.getX()*App.getScale()-App.getCameraOffsets().x,this._rect.getY()*App.getScale(),this._rect.getWidth(),this._rect.getHeight());
+            ctx.fillRect(
+                this._rect.getX()*App.getScale()-App.getCameraOffsets().x,
+                this._rect.getY()*App.getScale()-App.getCameraOffsets().y,
+                this._rect.getWidth()*App.getScale(),
+                this._rect.getHeight()*App.getScale()
+            );
         }
     }
 
     update(){
-
         if(this._rect.intersects(App.getState("gameState")._player._hitBox)){
             if(App.getResource("SynthChime4.wav").currentTime > 0){
                 App.getResource("SynthChime4.wav").currentTime = 0;
@@ -27,7 +35,5 @@ class PowerUp extends Entity{
             App.getState("gameState")._entities.delete(this._id);
             App.getState("gameState").addScore(10);
         }
-
     }
-
 }
